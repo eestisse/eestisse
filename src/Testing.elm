cca2860f5c89076d5183db9f0663617eeab9c20a
coinop-logan <coinop.logan@gmail.com>
2024-03-11 15:13:05 +0200

    add testing file

---
+++ src/Testing.elm
@@ -0,0 +1,32 @@
+module Testing exposing (..)
+
+import Types exposing (..)
+
+
+completedRequestExample : CompletedRequest
+completedRequestExample =
+    { inputText = ""
+    , translationResult =
+        Ok <|
+            { breakdown =
+                [ { estonian = "minu"
+                  , englishTranslation = "my"
+                  , maybeExplanation = Nothing
+                  }
+                , { estonian = "nimi"
+                  , englishTranslation = "is"
+                  , maybeExplanation = Just "some explanation here some explanation here some explanation here some explanation here some explanation here some explanation here "
+                  }
+                , { estonian = "on"
+                  , englishTranslation = "is"
+                  , maybeExplanation = Just "some explanation here some explanation here some explanation here some explanation here some explanation here some explanation here "
+                  }
+                , { estonian = "Logan"
+                  , englishTranslation = "is"
+                  , maybeExplanation = Just "some explanation here some explanation here some explanation here some explanation here some explanation here some explanation here "
+                  }
+                ]
+            , translation = "my name is Logan"
+            }
+    , maybeSelectedBreakdownPart = Nothing
+    }
