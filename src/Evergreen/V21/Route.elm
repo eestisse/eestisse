81b79dae6527984473f5ff667581e9a4c13df9e4
coinop-logan <coinop.logan@gmail.com>
2024-03-22 13:20:26 +0200

    Preparing for v21

---
+++ src/Evergreen/V21/Route.elm
@@ -0,0 +1,8 @@
+module Evergreen.V21.Route exposing (..)
+
+
+type Route
+    = Translate
+    | Landing
+    | Admin
+    | BadRoute
---
+++ src/Evergreen/V21/Types.elm
@@ -0,0 +1,118 @@
+module Evergreen.V21.Types exposing (..)
+
+import Browser
+import Browser.Navigation
+import Evergreen.V21.Route
+import Http
+import Lamdera
+import Set
+import Time
+import Url
+
+
+type GptAssistError
+    = OutOfCredits
+    | ApiProtocolError Http.Error
+    | GptDecodeError String
+    | GptExpressedError String
+
+
+type alias BreakdownPart =
+    { estonian : String
+    , englishTranslation : String
+    , maybeExplanation : Maybe String
+    }
+
+
+type alias Breakdown =
+    List BreakdownPart
+
+
+type EnglishOrEstonian
+    = English
+    | Estonian
+
+
+type alias Translation =
+    { breakdown : Breakdown
+    , translation : String
+    , translatedTo : EnglishOrEstonian
+    }
+
+
+type alias CompletedRequest =
+    { inputText : String
+    , translationResult : Result GptAssistError Translation
+    , maybeSelectedBreakdownPart : Maybe BreakdownPart
+    }
+
+
+type RequestState
+    = Waiting String Int
+    | RequestComplete CompletedRequest
+
+
+type TranslationPageModel
+    = InputtingText String
+    | RequestSent RequestState
+
+
+type SignupState
+    = Inactive
+    | Active String
+    | Submitting
+    | Submitted
+
+
+type alias FrontendModel =
+    { key : Browser.Navigation.Key
+    , route : Evergreen.V21.Route.Route
+    , translationPageModel : TranslationPageModel
+    , signupState : SignupState
+    , maybeImportantNumber : Maybe Int
+    }
+
+
+type alias BackendModel =
+    { nowish : Time.Posix
+    , publicCredits : Int
+    , emails : Set.Set String
+    , requests : List ( Time.Posix, String, Result GptAssistError Translation )
+    }
+
+
+type FrontendMsg
+    = UrlClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+    | NoOpFrontendMsg
+    | TextInputChanged String
+    | SubmitText String
+    | ShowExplanation BreakdownPart
+    | CycleLoadingAnimation
+    | EditTranslation String
+    | GotoRoute Evergreen.V21.Route.Route
+    | StartSignup
+    | SubmitSignup String
+    | SignupTextChanged String
+    | FetchImportantNumber
+
+
+type ToBackend
+    = NoOpToBackend
+    | SubmitTextForTranslation String
+    | SubmitEmail String
+    | RequestImportantNumber
+
+
+type BackendMsg
+    = NoOpBackendMsg
+    | GptResponseReceived Lamdera.ClientId String (Result Http.Error String)
+    | AddPublicCredits
+    | UpdateNow Time.Posix
+
+
+type ToFrontend
+    = NoOpToFrontend
+    | TranslationResult String (Result GptAssistError Translation)
+    | EmailSubmitAck
+    | ImportantNumber Int
