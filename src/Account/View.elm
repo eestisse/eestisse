9af28cd77f18446404a9e378d829a3074872ef64
coinop-logan <coinop.logan@gmail.com>
2024-04-25 13:51:23 +0300

    some real basic structure

--- src/Account/View.elm
+++ src/Account/View.elm
@@ -9,42 +9,42 @@ import Types exposing (..)
 
 page : DisplayProfile -> FrontendModel -> Element FrontendMsg
 page dProfile model =
+    if maybeFrontendUserHasActiveMembership model.maybeAuthedUserInfo then
+        Element.text "oh nice brudda nice wan"
+
+    else
+        viewOfferAndActionButtons dProfile model.maybeAuthedUserInfo
+
+
+viewOfferAndActionButtons : DisplayProfile -> Maybe FrontendUserInfo -> Element FrontendMsg
+viewOfferAndActionButtons dProfile maybeAuthedUserInfo =
     Element.column
-        [ Element.centerX
-        , Element.centerY
+        [ Element.width Element.fill
         , Element.spacing 20
         ]
-        [ case model.maybeAuthedUserInfo of
-            Nothing ->
-                mainActionButton "login with google oauth" <|
-                    Just <|
-                        AuthSigninRequested { methodId = "OAuthGoogle", username = Nothing }
-
-            Just userInfo ->
-                Element.row []
-                    [ Element.text <| "Logged in as " ++ userInfo.email
-                    , mainActionButton "log out" <|
-                        Just <|
-                            Logout
-                    ]
-        , Element.row
-            [ Element.spacing 10 ]
-            [ mainActionButton "How much do you like me??" <|
-                Just <|
-                    AskHowMuchYouLikeMe
-            , viewHowMuchYouLikeMe model.backendModelAffection
-            ]
-        , case model.maybeAuthedUserInfo of
-            Just userInfo ->
-                mainActionButton "take my money" <| Just <| TriggerStripePayment userInfo.id
-
-            Nothing ->
-                Element.none
+        [ viewOffer dProfile
+        , viewAccountInfoAndActionButton dProfile maybeAuthedUserInfo
         ]
 
 
-viewHowMuchYouLikeMe : Maybe String -> Element FrontendMsg
-viewHowMuchYouLikeMe maybeAffection =
-    maybeAffection
-        |> Maybe.withDefault "idk"
-        |> Element.text
+viewOffer : DisplayProfile -> Element FrontendMsg
+viewOffer dProfile =
+    Element.text "how about please give me money"
+
+
+viewAccountInfoAndActionButton : DisplayProfile -> Maybe FrontendUserInfo -> Element FrontendMsg
+viewAccountInfoAndActionButton dProfile maybeAuthedUserInfo =
+    case maybeAuthedUserInfo of
+        Nothing ->
+            mainActionButton "login with google oauth" <|
+                Just <|
+                    AuthSigninRequested { methodId = "OAuthGoogle", username = Nothing }
+
+        Just userInfo ->
+            Element.column
+                [ Element.centerX
+                , Element.spacing 10
+                ]
+                [ Element.text <| "Logged in as " ++ userInfo.email
+                , mainActionButton "Buy with Stripe" <| Just <| TriggerStripePayment userInfo.id
+                ]
